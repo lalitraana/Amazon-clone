444e587028b43ade61a81b04655c4c11758b361e
Lalit Singh <lalitraana1993@gmail.com>
2020-12-31 18:41:45 +0530

    header created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import React from "react";
 import "./App.css";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home";
+import "./Header.css";
 function App() {
-  return <div className="App">amazon clone</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+function Home() {
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+        alt="home image"
+      />
+    </div>
+  );
+}
+
+export default Home;
